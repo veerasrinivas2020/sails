1afe5f4e91526fe249b89de1afdce70391643881
veera srinivas <srinivas_raneru2003@yahoo.com>
2020-11-30 09:54:25 +0530

    after password

---
+++ api/controllers/StduentController.js
@@ -0,0 +1,12 @@
+/**
+ * StduentController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
